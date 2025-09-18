5d3c9172baede544b841f8e24a4ba53c2615e7ce
Sebas <139570060+saccdev12@users.noreply.github.com>
2025-09-18 12:09:16 +0000

    little changes

--- eda-2do-semestre/enum/comandos-lista/funciones.cpp
+++ eda-2do-semestre/enum/comandos-lista/funciones.cpp
@@ -3,39 +3,11 @@
 using namespace std;
 #include "definicion.h"
 
-void muestroRetorno(tipoRetorno retorno){
-	switch (retorno){
-	case OK:
-		cout << "OK";
-		break;
-	case ERROR:
-		cout << "ERROR";
-		break;
-	case NO_IMPLEMENTADO:
-		cout << "NO IMPLEMENTADO";
-	}
-	cout << endl;
-}
-
-	
 void finalizo(){
 	cout << "Gracias por utilizar comando 1.0 - Todos los derechos reservados " << endl;
-}
-
-
-tipoRetorno resultadoCrear(lista l){
-	return NO_IMPLEMENTADO;
-}
-tipoRetorno resultadoInsertar(lista l){
-	return NO_IMPLEMENTADO;
-}
-tipoRetorno resultadoMostrar(lista l){
-	return NO_IMPLEMENTADO;
-}
-tipoRetorno resultadoContarNodos(lista l){
-	return NO_IMPLEMENTADO;
 }
 
 void help(){
 	cout << "Comando permitidos: crear - mostrar - insertar - contarNodos - limpiar - ayuda - fin"<<endl;
 }
+
