2f3b3421ca6087389535437f4de27c1b31afb2a7
Sebas <139570060+saccdev12@users.noreply.github.com>
2026-04-02 16:17:13 +0000

    practicas poo creadas

---
+++ pa-3er-semestre/practicas-poo/athlete.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+class athlete {
+     
+       string name;
+       string sport; 
+       int age;
+       int power;
+       int speed;
+
+     public:
+       //constructor, se usa para inicializar los atributos de la clase.
+       athlete (){
+          string name = "Anonymus";
+          string sport = "Unknown";
+          int age = 0;
+          int power = 100;
+          int speed = 30;
+       }
+
+};
+
+
+
+
+
+void main (){
+
+}
