ba51ddb172c0200cf05d8e245b7ada4df2e46783
Sebas <139570060+saccdev12@users.noreply.github.com>
2026-04-05 14:57:12 +0000

    std:: removed

--- pa-3er-semestre/ejercicios/laboratorio0/DTRefer.h
+++ pa-3er-semestre/ejercicios/laboratorio0/DTRefer.h
@@ -5,21 +5,23 @@
 #include <set>
 #include "DTFecha.h"
 
+using namespace std;
+
 class DTRefer {
     private:
-        std::string DOI;
-        std::string titulo;
-        std::set<std::string> autores; //los autores NO son objetos, son strings.
+        string DOI;
+        string titulo;
+        set<std::string> autores; //los autores NO son objetos, son strings.
 
     public:
        DTRefer();
        DTRefer(std::string DOI, std::string titulo, DTFecha fecha, std::set<std::string> autores); //constructor completo.
        
        //Getters
-       std::string getDOI() const;
-       std::string getTitulo() const;
+       string getDOI() const;
+       string getTitulo() const;
        DTFecha getFecha() const;
-       std::set<std::string> getAutores() const;
+       set<std::string> getAutores() const;
         
        //Sobrecarga del operador, accede a atributos que son privados, lo usamos para imprimir el objeto.
        friend std::ostream& operator<<(std::ostream& os, const DTRefer& referencia);
